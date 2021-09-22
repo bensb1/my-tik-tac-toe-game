184518df75b3306b046ad306fe32507d47487095
bensb1 <162532@chrome.escg.ac.uk>
2021-09-22 16:00:34 +0100

    first push

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public GameObject mCellPrefab;
+    private Cell[] mcells = new Cell[9];
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void Build()
+    {
+        for (int i = 0; i <= 8; i++)
+        {
+            GameObject newCell = Instantiate(mCellPrefab, transform);
+            mcells[i] = newCell.GetComponent<Cell>();
+        }
+    }
+
+  
+}
